0f6c1deb6619628c11fa8c9dfdc74ab4a9260288
bfcamara <bruno.camara@gmail.com>
2015-12-21 08:18:51 +0000

    generate vso-task-lib node_modules in build tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,11 @@
 var url = require('url');
+var shell = require('shelljs')
+var path = require('path');
+var semver = require('semver');
+var fs = require('fs');
+var cp = require('child_process');
+
+var NPM_MIN_VER = '3.0.0';
 
 module.exports = function (grunt) {
 
@@ -94,14 +101,37 @@ module.exports = function (grunt) {
       },
     }
   }
+  
+  var linkBuildTasksConfig = {
+    modules: {
+      buildTasksDir: '<%= dirs.output.buildTasks %>',
+      tempFolder: '_temp',
+      tasksToLink: ['PackageExtension', 'PublishExtension'],
+      pkg: {
+        "name": "temp",
+        "version": "1.0.0",
+        "description": "temp to avoid warnings",
+        "main": "index.js",
+        "dependencies": {},
+        "devDependencies": {},
+        "repository": "http://norepo/but/nowarning",
+        "scripts": {
+          "test": "echo \"Error: no test specified\" && exit 1"
+        },
+        "author": "",
+        "license": "MIT"
+      }
+    }  
+  };
 
   grunt.config('typescript', typeScriptConfig);
   grunt.config('copy', copyConfig);
   grunt.config('prepManifest', prepManifestConfig);
   grunt.config('watch', watchConfig);
+  grunt.config('linkBuildTasks', linkBuildTasksConfig);
     
   // Register build task
-  grunt.registerTask('build', ['clean', 'typescript', 'copy']);
+  grunt.registerTask('build', ['clean', 'typescript', 'copy', 'linkBuildTasks']);
   
   // Register custom "prep manifest" task
   grunt.task.registerMultiTask('prepManifest', 'Prep the manifest by updating base URI and publisher', function () {
@@ -127,4 +157,47 @@ module.exports = function (grunt) {
   grunt.registerTask('prepPackage', ['build', 'prepManifest']);
 
   grunt.registerTask('default', ['build']);
+  
+  grunt.registerMultiTask('linkBuildTasks', 'Add vso-tak-lib to each build task', function() {
+    var tempPath = path.join(this.data.buildTasksDir, this.data.tempFolder); 
+    var node_modulesPath = path.join(tempPath, 'node_modules');
+    
+    grunt.log.writeln('Getting latest vso-task-lib');
+    shell.mkdir('-p', node_modulesPath);
+
+    fs.writeFileSync(path.join(tempPath, 'package.json'), JSON.stringify(this.data.pkg, null, 2));
+  
+    shell.pushd(tempPath);
+  
+    var npmPath = shell.which('npm');
+    if (!npmPath) {
+      throw new Error('npm not found.  ensure npm 3 or greater is installed');
+    }
+  
+    // check npm version
+    var s = cp.execSync('"' + npmPath + '" --version');
+    var ver = s.toString().replace(/[\n\r]+/g, '')
+    console.log('version: "' + ver + '"');
+  
+    if (semver.lt(ver, NPM_MIN_VER)) {
+      throw new Error('NPM version must be at least ' + NPM_MIN_VER + '. Found ' + ver);
+    }
+  
+    var cmdline = '"' + npmPath + '" install vso-task-lib';
+  
+    var res = cp.execSync(cmdline); 
+    grunt.log.writeln(res.toString());	
+    shell.popd();
+    if (res.status > 0) {
+      throw new Error('npm failed with code of ' + res.status);
+    }
+    
+    // For each build task copy the node_modules
+    this.data.tasksToLink.forEach(function(taskName) {
+        shell.cp('-R', node_modulesPath, path.join(this.data.buildTasksDir, taskName));
+    }, this);
+    
+    // Remove the _temp folder
+    shell.rm('-R', tempPath);
+  });
 };
