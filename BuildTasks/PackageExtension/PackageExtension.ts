11ce13d8290d4f97b4bb0cbdfade18968880d366
bfcamara <bruno.camara@gmail.com>
2016-02-08 10:36:55 +0000

    sopt demanding tfx and install it on the fly

--- BuildTasks/PackageExtension/PackageExtension.ts
+++ BuildTasks/PackageExtension/PackageExtension.ts
@@ -1,43 +1,46 @@
 ///<reference path="../typings/main.d.ts" />
 import tl = require('vsts-task-lib/task');
+import common = require("./common");
 
-var tfx = tl.createToolRunner(tl.which('tfx', true));
-tfx.arg("extension");
-tfx.arg("create");
+common.runTfx(tfx => {
+    tfx.arg("extension");
+    tfx.arg("create");
 
-var publisher = tl.getInput('publisher', false);
-if (publisher) {
-    tfx.arg('--publisher');
-    tfx.arg(publisher);
-}
+    var publisher = tl.getInput('publisher', false);
+    if (publisher) {
+        tfx.arg('--publisher');
+        tfx.arg(publisher);
+    }
 
-var rootFolder = tl.getInput('rootFolder', false);
-if (rootFolder) {
-    tfx.arg('--root');
-    tfx.arg(rootFolder);
-}
+    var rootFolder = tl.getInput('rootFolder', false);
+    if (rootFolder) {
+        tfx.arg('--root');
+        tfx.arg(rootFolder);
+    }
 
-var globsManifest = tl.getInput('patternManifest', false);
-if (globsManifest) {
-    tfx.arg('--manifest-globs');
-    tfx.arg(globsManifest);
-}
+    var globsManifest = tl.getInput('patternManifest', false);
+    if (globsManifest) {
+        tfx.arg('--manifest-globs');
+        tfx.arg(globsManifest);
+    }
 
-var outputPath = tl.getInput('outputPath', false);
-if (outputPath) {
-    tfx.arg('--output-path');
-    tfx.arg(outputPath);
-}
+    var outputPath = tl.getInput('outputPath', false);
+    if (outputPath) {
+        tfx.arg('--output-path');
+        tfx.arg(outputPath);
+    }
 
-tfx.arg(tl.getInput('arguments', false));
+    tfx.arg(tl.getInput('arguments', false));
 
-var cwd = tl.getInput('cwd', false);
-if (cwd) {
-    tl.cd(cwd);
-}
+    var cwd = tl.getInput('cwd', false);
+    if (cwd) {
+        tl.cd(cwd);
+    }
 
-tfx.exec().then(code => {
-    tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
-}).fail(err => {
-    tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
-});
+    tfx.exec().then(code => {
+        tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
+    }).fail(err => {
+        tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+    });    
+})
+
--- BuildTasks/PublishExtension/PublishExtension.ts
+++ BuildTasks/PublishExtension/PublishExtension.ts
@@ -1,5 +1,6 @@
 ///<reference path="../typings/main.d.ts" />
 import tl = require('vsts-task-lib/task');
+import common = require('./common');
 
 function getEndpointDetails(inputFieldName) {
     var errorMessage = "Could not decode the marketplace endpoint. Please ensure you are running the latest VSTS agent";
@@ -23,53 +24,54 @@ function getEndpointDetails(inputFieldName) {
     };
 }
 
-var tfx = tl.createToolRunner(tl.which('tfx', true));
-tfx.arg("extension publish");
+common.runTfx(tfx => {
+    tfx.arg("extension publish");
 
-// Read gallery endpoint
-var galleryEndpoint = getEndpointDetails('connectedServiceName');
-tfx.arg('--token');
-tfx.arg(galleryEndpoint.token);
+    // Read gallery endpoint
+    var galleryEndpoint = getEndpointDetails('connectedServiceName');
+    tfx.arg('--token');
+    tfx.arg(galleryEndpoint.token);
 
-tfx.arg('--service-url');
-tfx.arg(galleryEndpoint.url);
+    tfx.arg('--service-url');
+    tfx.arg(galleryEndpoint.url);
 
-// Read file type
-var fileType = tl.getInput('fileType', true);
-if (fileType == "manifest") {
-    var rootFolder = tl.getInput('rootFolder', false);
-    if (rootFolder) {
-        tfx.arg('--root');
-        tfx.arg(rootFolder);
+    // Read file type
+    var fileType = tl.getInput('fileType', true);
+    if (fileType == "manifest") {
+        var rootFolder = tl.getInput('rootFolder', false);
+        if (rootFolder) {
+            tfx.arg('--root');
+            tfx.arg(rootFolder);
+        }
+
+        var globsManifest = tl.getInput('patternManifest', false);
+        if (globsManifest) {
+            tfx.arg('--manifest-globs');
+            tfx.arg(globsManifest);
+        }
+    } else {
+        var vsixFile = tl.getInput('vsixFile', true);
+        tfx.arg('--vsix');
+        tfx.arg(vsixFile);
     }
 
-    var globsManifest = tl.getInput('patternManifest', false);
-    if (globsManifest) {
-        tfx.arg('--manifest-globs');
-        tfx.arg(globsManifest);
+    // Read publisher
+    var publisher = tl.getInput('publisher', false);
+    if (publisher) {
+        tfx.arg('--publisher');
+        tfx.arg(publisher);
     }
-} else {
-    var vsixFile = tl.getInput('vsixFile', true);
-    tfx.arg('--vsix');
-    tfx.arg(vsixFile);
-}
 
-// Read publisher
-var publisher = tl.getInput('publisher', false);
-if (publisher) {
-    tfx.arg('--publisher');
-    tfx.arg(publisher);
-}
+    tfx.arg(tl.getInput('arguments', false));
 
-tfx.arg(tl.getInput('arguments', false));
+    var cwd = tl.getInput('cwd', false);
+    if (cwd) {
+        tl.cd(cwd);
+    }
 
-var cwd = tl.getInput('cwd', false);
-if (cwd) {
-    tl.cd(cwd);
-}
-
-tfx.exec().then(code => {
-    tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
-}).fail(err => {
-    tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+    tfx.exec().then(code => {
+        tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
+    }).fail(err => {
+        tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+    });
 });
