209179fef6430abcf3d29b68cdc9327e7d9aa958
bfcamara <bruno.camara@gmail.com>
2016-02-24 10:20:47 +0000

    allow to override manifest values for extension id, name and visibility

--- BuildTasks/Common/Common.ts
+++ BuildTasks/Common/Common.ts
@@ -4,44 +4,117 @@
 // This file is copied to each build task.
 // Any change should be made in the file that is in the Common folder
 
-import os = require('os');
-import tl = require('vsts-task-lib/task');
-import trl = require('vsts-task-lib/toolrunner');
-import ToolRunner = trl.ToolRunner; 
+import os = require("os");
+import path = require("path");
+import fs = require("fs");
+import tl = require("vsts-task-lib/task");
+import trl = require("vsts-task-lib/toolrunner");
+import ToolRunner = trl.ToolRunner;
 
-export function runTfx(cmd : (tfx: ToolRunner) => void) {
-    var tfx : ToolRunner;
-    
+function writeBuildTempFile(taskName: string, data: any): string {
+    const buildNumber = tl.getVariable("Build.BuildNumber");
+    let tempFile: string;
+    do {
+        // Let's add a random suffix
+        let randomSuffix = Math.random().toFixed(6);
+        tempFile = path.join(os.tmpdir(), `${taskName}-${buildNumber}-${randomSuffix}.tmp`);
+    } while (fs.existsSync(tempFile));
+
+    tl.debug(`Generating Build temp file: ${tempFile}`);
+    fs.writeFileSync(tempFile, data);
+
+    return tempFile;
+}
+
+function deleteBuildTempFile(tempFile: string) {
+    if (tempFile && fs.existsSync(tempFile)) {
+        tl.debug(`Deleting temp file: ${tempFile}`);
+        fs.unlinkSync(tempFile);
+    }
+}
+/**
+ * Set manifest related arguments for "tfx extension" command, such as
+ * the  --root or the --manifest-globs switches.
+ * @param  {ToolRunner} tfx
+ * @returns {() => void} Cleaner function that the caller should use to cleanup temporary files created to be used as arguments
+ */
+export function setTfxManifestArguments(tfx: ToolRunner): (() => void) {
+    const rootFolder = tl.getInput("rootFolder", false);
+    tfx.argIf(rootFolder, ["--root", rootFolder]);
+
+    const globsManifest = tl.getInput("patternManifest", false);
+    tfx.argIf(globsManifest, ["--manifest-globs", globsManifest]);
+
+    // Overrides manifest file
+    const publisher = tl.getInput("publisher", false);
+    tfx.argIf(publisher, ["--publisher", publisher]);
+
+    const extensionId = tl.getInput("extensionId", false);
+    tfx.argIf(extensionId, ["--extension-id", extensionId]);
+
+    let jsonOverrides: any;
+    const extensionName = tl.getInput("extensionName", false);
+    if (extensionName) {
+        tl.debug(`Overriding extension name to: ${extensionName}`);
+        jsonOverrides = (jsonOverrides || {});
+        jsonOverrides.name = extensionName;
+    }
+
+    const extensionVisibility = tl.getInput("extensionVisibility", false);
+    if (extensionVisibility && extensionVisibility !== "default") {
+        tl.debug(`Overriding extension visibility to: ${extensionVisibility}`);
+        jsonOverrides = (jsonOverrides || {});
+        jsonOverrides.public = (extensionVisibility === "public");
+    }
+
+    let overrideFilePath: string;
+    if (jsonOverrides) {
+        // Generate a temp file
+        overrideFilePath = writeBuildTempFile("PackageTask", JSON.stringify(jsonOverrides));
+        tl.debug(`Generated a JSON temp file to override manifest values Path: ${overrideFilePath}`);
+
+        tfx.arg(["--overrides-file", overrideFilePath]);
+    }
+
+    return () => deleteBuildTempFile(overrideFilePath);
+}
+/**
+ * Run a tfx command by ensuring that "tfx" exists, installing it on the fly if needed.
+ * @param  {(tfx:ToolRunner)=>void} cmd
+ */
+export function runTfx(cmd: (tfx: ToolRunner) => void) {
+    let tfx: ToolRunner;
+
     // Check the global tfx
-    var tfxPath = tl.which('tfx');
+    let tfxPath = tl.which("tfx");
     if (tfxPath) {
         tfx = tl.createToolRunner(tfxPath);
         cmd(tfx);
         return;
     }
-        
+
     // Check the local tfx (due a previous installation)
-    var tfxLocalPath = 'node_modules/.bin/tfx';
+    let tfxLocalPath = "node_modules/.bin/tfx";
     // On windows we are looking for tfx.cmd
-    if (os.platform().toLowerCase().indexOf('win') >= 0) {
-        tfxLocalPath += '.cmd';
+    if (os.platform().toLowerCase().indexOf("win") >= 0) {
+        tfxLocalPath += ".cmd";
     }
-    
+
     tfxPath = tl.which(tfxLocalPath);
     if (tfxPath) {
         tfx = tl.createToolRunner(tfxPath);
         cmd(tfx);
         return;
-    }    
-    
-    console.log('Could not find tfx command. Preparing to install it');
-    
-    var npm = tl.createToolRunner(tl.which('npm', true));
-    npm.arg('install tfx-cli');
-    
+    }
+
+    console.log("Could not find tfx command. Preparing to install it");
+
+    const npm = tl.createToolRunner(tl.which("npm", true));
+    npm.arg(["install", "tfx-cli"]);
+
     npm.exec().then(code => {
       tfx = tl.createToolRunner(tl.which(tfxLocalPath, true));
-      cmd(tfx);  
+      cmd(tfx);
     }).fail(err => {
         tl.setResult(tl.TaskResult.Failed, `Error installing tfx: ${err}`);
     });
