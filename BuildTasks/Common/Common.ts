0a6f8e51a89adad237afdcee5a7daee5ff43f611
Bruno Câmara <bruca@microsoft.com>
2016-03-16 11:55:59 +0000

    do not check tfx globally by default install tfx locally in the build dir path

--- BuildTasks/Common/Common.ts
+++ BuildTasks/Common/Common.ts
@@ -84,36 +84,70 @@ export function setTfxManifestArguments(tfx: ToolRunner): (() => void) {
  */
 export function runTfx(cmd: (tfx: ToolRunner) => void) {
     let tfx: ToolRunner;
+    let tfxPath: string;
 
-    // Check the global tfx
-    let tfxPath = tl.which("tfx");
-    if (tfxPath) {
-        tfx = tl.createToolRunner(tfxPath);
-        cmd(tfx);
-        return;
+    const checkTfxGlobalVar = tl.getVariable("vstsDevTools.buildTasks.checkGlobalTfx");
+    if (checkTfxGlobalVar && checkTfxGlobalVar.toLowerCase() !== "false") {
+        console.log("Checking tfx globally");
+        tfxPath = tl.which("tfx");
+        if (tfxPath) {
+            console.log(`Found tfx globally ${tfxPath}`);
+            tfx = tl.createToolRunner(tfxPath);
+            cmd(tfx);
+            return;
+        }
     }
 
-    // Check the local tfx (due a previous installation)
+    // Check the local tfx to see if it is included in task
     let tfxLocalPath = "node_modules/.bin/tfx";
     // On windows we are looking for tfx.cmd
     if (os.platform().toLowerCase().indexOf("win") >= 0) {
         tfxLocalPath += ".cmd";
     }
 
+    console.log("Checking tfx in current task folder");
     tfxPath = tl.which(tfxLocalPath);
     if (tfxPath) {
+        console.log(`Found tfx in current task folder ${tfxPath}`);
         tfx = tl.createToolRunner(tfxPath);
         cmd(tfx);
         return;
     }
 
-    console.log("Could not find tfx command. Preparing to install it");
+    // Check tfx in a build installation path
+    const tfxInstallBasePath = path.join(tl.getVariable("Agent.BuildDirectory"), "_tools", "tfx-cli");
+    const tfxInstallPath = path.join(tfxInstallBasePath, tl.getVariable("Build.DefinitionVersion"));
+    const tfxBuildPath = path.join(tfxInstallPath, tfxLocalPath);
+
+    console.log(`Checking tfx in build path (${tfxBuildPath})`);
+    tfxPath = tl.which(tfxBuildPath);
+    if (tfxPath) {
+        console.log(`Found tfx in ${tfxPath}`);
+        tfx = tl.createToolRunner(tfxPath);
+        cmd(tfx);
+        return;
+    }
+
+
+    console.log(`Could not find tfx command. Preparing to install it in ${tfxInstallPath}`);
 
     const npm = tl.createToolRunner(tl.which("npm", true));
     npm.arg(["install", "tfx-cli"]);
 
-    npm.exec().then(code => {
-      tfx = tl.createToolRunner(tl.which(tfxLocalPath, true));
+    // Ensure tools dir exists in build
+    if (!tl.exist(tfxInstallPath)) {
+        // We are about to install tfx-cli for a new version definition. Delete previous versions to deleteBuildTempFile
+        if (tl.exist(tfxInstallBasePath)) {
+            console.log("Removing tfx-cli installed in previous versions");
+            tl.rmRF(tfxInstallBasePath, true);
+        }
+
+        tl.mkdirP(tfxInstallPath);
+    }
+
+    const tfxInstallOptions = <trl.IExecOptions> { cwd: tfxInstallPath };
+    npm.exec(tfxInstallOptions).then(code => {
+      tfx = tl.createToolRunner(tl.which(tfxBuildPath, true));
       cmd(tfx);
     }).fail(err => {
         tl.setResult(tl.TaskResult.Failed, `Error installing tfx: ${err}`);
@@ -122,7 +156,7 @@ export function runTfx(cmd: (tfx: ToolRunner) => void) {
 
 /**
  * Get the Marketplace endpoint details to be used while publishing or installing an extension.
- * 
+ *
  * @param  {string="connectedServiceName"} inputFieldName
  * @returns string
  */
