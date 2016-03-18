190378ab52df4a7d1bbe7e534f445954f71bf4c3
Bruno Câmara <bruca@microsoft.com>
2016-03-18 19:59:33 +0000

    fix some tslint warnings use a TfxDebugStream instead of silent flag to allow to see tfx-cli errors in debug

--- BuildTasks/ExtensionVersion/ExtensionVersion.ts
+++ BuildTasks/ExtensionVersion/ExtensionVersion.ts
@@ -1,6 +1,14 @@
 ///<reference path="../typings/main.d.ts" />
 import tl = require("vsts-task-lib/task");
 import common = require("./common");
+import stream = require("stream");
+
+class TfxDebugStream extends stream.Writable {
+    _write(chunk: any, enc: string, cb: Function) {
+        tl.debug(chunk);
+        cb();
+    }
+}
 
 common.runTfx(tfx => {
     tfx.arg(["extension", "show", "--json"]);
@@ -16,7 +24,7 @@ common.runTfx(tfx => {
 
     const outputVariable = tl.getInput("outputVariable", false);
     const versionAction = tl.getInput("versionAction", false);
-    
+
     // Aditional arguments
     tfx.arg(tl.getInput("arguments", false));
 
@@ -26,20 +34,20 @@ common.runTfx(tfx => {
         tl.cd(cwd);
     }
 
-    let output = ""
-    tfx.on("stdout", (data) => { output += data })
-    
-    tfx.exec(<any>{ silent: true }).then(code => {
+    let output = "";
+    tfx.on("stdout", (data) => output += data);
+
+    tfx.exec(<any>{ outStream: new TfxDebugStream() }).then(code => {
         const json = JSON.parse(output);
-        let version = json.versions[json.versions.length-1].version;
-        
-        tl._writeLine(`Latest version   : ${version}.`)
-        tl._writeLine(`Requested action : ${versionAction}.`)
-        
+        let version = json.versions[json.versions.length - 1].version;
+
+        tl._writeLine(`Latest version   : ${version}.`);
+        tl._writeLine(`Requested action : ${versionAction}.`);
+
         if (versionAction !== "None") {
             let versionparts = version.Split(".");
             switch (versionAction) {
-                case "Major": 
+                case "Major":
                     versionparts = [+versionparts[0] + 1, 0, 0];
                     break;
                 case "Minor":
@@ -50,12 +58,12 @@ common.runTfx(tfx => {
                     break;
             }
             version = versionparts.Join(".");
-            tl._writeLine(`Updated to       : ${version}.`)
+            tl._writeLine(`Updated to       : ${version}.`);
         }
-        
+
         tl.setVariable(outputVariable, version);
         tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
     }).fail(err => {
         tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
-    })
+    });
 });
