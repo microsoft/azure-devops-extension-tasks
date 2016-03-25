7b1b632c95594ed6232d2b625344c948ba94c1f5
Jesse Houwing <jesse.houwing@gmail.com>
2016-03-25 13:45:53 +0100

    Adds Share task
    Adds an extra task that will share an extension using the extensionid and publisher or using an existing vsix file.

---
+++ BuildTasks/ShareExtension/ShareExtension.ts
@@ -0,0 +1,47 @@
+///<reference path="../typings/main.d.ts" />
+import tl = require("vsts-task-lib/task");
+import common = require("./common");
+
+common.runTfx(tfx => {
+    tfx.arg(["extension", "share"]);
+    const method = tl.getInput("method", true);
+    
+    // Read gallery endpoint
+    const galleryEndpoint = common.getMarketplaceEndpointDetails();
+    tfx.arg(["--token", galleryEndpoint.token]);
+    tfx.arg(["--service-url", galleryEndpoint.url]);
+
+    switch (method){
+        case "id":
+        // Extension name
+        tfx.arg(["--publisher", tl.getInput("publisherId", true)]);
+        tfx.arg(["--extension-id", tl.getInput("extensionId", true)]);
+        break;
+        
+        case "vsix":
+        tfx.arg(["--vsix", tl.getInput("vsixFile", true)]);
+        break;
+    }
+
+    // Installation targets
+    const accountsArg = tl.getInput("accounts", true);
+
+    // Sanitize accounts list
+    const accounts = accountsArg.split(",").map(a => a.replace(/\s/g, "")).filter(a => a.length > 0);
+    tfx.arg(["--share-with", ...accounts]);
+
+    // Aditional arguments
+    tfx.arg(tl.getInput("arguments", false));
+
+    // Set working folder
+    const cwd = tl.getInput("cwd", false);
+    if (cwd) {
+        tl.cd(cwd);
+    }
+
+    tfx.exec().then(code => {
+        tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
+    }).fail(err => {
+        tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+    });
+});
