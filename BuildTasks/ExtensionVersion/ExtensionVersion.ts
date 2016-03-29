088e954aaedd688638360b95ac00eb3c417bf70b
Jesse Houwing <jesse.houwing@gmail.com>
2016-03-29 16:53:31 +0200

    Allow version override from queue screen
    Adds a variable to the Query Version experience which will override the value coming from the marketplace regardless.
    
    This will allow a one-time override on the queue window to increase a major or minor version, while normally increasing the Patch.

--- BuildTasks/ExtensionVersion/ExtensionVersion.ts
+++ BuildTasks/ExtensionVersion/ExtensionVersion.ts
@@ -3,58 +3,64 @@ import tl = require("vsts-task-lib/task");
 import common = require("./common");
 import stream = require("stream");
 
-common.runTfx(tfx => {
-    tfx.arg(["extension", "show", "--json"]);
-    const outputVariable = tl.getInput("outputVariable", true);
+const extensionVersionOverride = tl.getInput("extensionVersionOverride", false);
+const outputVariable = tl.getInput("outputVariable", true);
 
-    // Read gallery endpoint
-    const galleryEndpoint = common.getMarketplaceEndpointDetails();
-    tfx.arg(["--token", galleryEndpoint.token]);
-    tfx.arg(["--service-url", galleryEndpoint.url]);
+if (extensionVersionOverride) {
+    tl.setVariable(outputVariable, extensionVersionOverride);
+} else {
+    common.runTfx(tfx => {
+        tfx.arg(["extension", "show", "--json"]);
 
-    // Extension name
-    tfx.arg(["--publisher", tl.getInput("publisherId", true)]);
-    tfx.arg(["--extension-id", tl.getInput("extensionId", true)]);
+        // Read gallery endpoint
+        const galleryEndpoint = common.getMarketplaceEndpointDetails();
+        tfx.arg(["--token", galleryEndpoint.token]);
+        tfx.arg(["--service-url", galleryEndpoint.url]);
 
-    const versionAction = tl.getInput("versionAction", false);
+        // Extension name
+        tfx.arg(["--publisher", tl.getInput("publisherId", true)]);
+        tfx.arg(["--extension-id", tl.getInput("extensionId", true)]);
 
-    // Aditional arguments
-    tfx.arg(tl.getInput("arguments", false));
+        const versionAction = tl.getInput("versionAction", false);
 
-    // Set working folder
-    const cwd = tl.getInput("cwd", false);
-    if (cwd) {
-        tl.cd(cwd);
-    }
+        // Aditional arguments
+        tfx.arg(tl.getInput("arguments", false));
 
-    const outputStream = new common.TfxJsonOutputStream();
-    tfx.exec(<any>{ outStream: outputStream, failOnStdErr: true }).then(code => {
-        const json = JSON.parse(outputStream.jsonString);
-        let version: string = json.versions[json.versions.length - 1].version;
-
-        tl._writeLine(`Latest version   : ${version}.`);
-        tl._writeLine(`Requested action : ${versionAction}.`);
-
-        if (versionAction !== "None") {
-            let versionparts: number[] = version.split(".").map(v => +v);
-            switch (versionAction) {
-                case "Major":
-                    versionparts = [versionparts[0] + 1, 0, 0];
-                    break;
-                case "Minor":
-                    versionparts = [versionparts[0], versionparts[1] + 1, 0];
-                    break;
-                case "Patch":
-                    versionparts = [versionparts[0], versionparts[1], versionparts[2] + 1];
-                    break;
-            }
-            version = versionparts.join(".");
-            tl._writeLine(`Updated to       : ${version}.`);
+        // Set working folder
+        const cwd = tl.getInput("cwd", false);
+        if (cwd) {
+            tl.cd(cwd);
         }
 
-        tl.setVariable(outputVariable, version);
-        tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
-    }).fail(err => {
-        tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+        const outputStream = new common.TfxJsonOutputStream();
+        tfx.exec(<any>{ outStream: outputStream, failOnStdErr: true }).then(code => {
+            const json = JSON.parse(outputStream.jsonString);
+            let version: string = json.versions[json.versions.length - 1].version;
+
+            tl._writeLine(`Latest version   : ${version}.`);
+            tl._writeLine(`Requested action : ${versionAction}.`);
+
+            if (versionAction !== "None") {
+                let versionparts: number[] = version.split(".").map(v => +v);
+                switch (versionAction) {
+                    case "Major":
+                        versionparts = [versionparts[0] + 1, 0, 0];
+                        break;
+                    case "Minor":
+                        versionparts = [versionparts[0], versionparts[1] + 1, 0];
+                        break;
+                    case "Patch":
+                        versionparts = [versionparts[0], versionparts[1], versionparts[2] + 1];
+                        break;
+                }
+                version = versionparts.join(".");
+                tl._writeLine(`Updated to       : ${version}.`);
+            }
+
+            tl.setVariable(outputVariable, version);
+            tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
+        }).fail(err => {
+            tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+        });
     });
-});
+}
