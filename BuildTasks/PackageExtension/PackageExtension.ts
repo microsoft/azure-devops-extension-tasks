364ee02e059211d4be9215f58c0054d7a07e0c6c
Jesse Houwing <jesse.houwing@gmail.com>
2018-03-05 11:51:13 -0800

    support multiple task versions

--- BuildTasks/PackageExtension/PackageExtension.ts
+++ BuildTasks/PackageExtension/PackageExtension.ts
@@ -3,36 +3,42 @@ import * as common from "./common";
 
 common.setProxy();
 
-common.runTfx(tfx => {
-    tfx.arg(["extension", "create", "--json"]);
-    const outputVariable = tl.getInput("outputVariable", false);
+async function run() {
+    try {
+        common.runTfx(async tfx => {
+            tfx.arg(["extension", "create", "--json"]);
+            const outputVariable = tl.getInput("outputVariable", false);
 
-    // Set tfx manifest arguments
-    const cleanupTfxArgs = common.validateAndSetTfxManifestArguments(tfx);
+            // Set tfx manifest arguments
+            const cleanupTfxArgs = common.validateAndSetTfxManifestArguments(tfx);
 
-    // Set vsix output path
-    const outputPath = tl.getInput("outputPath", false);
-    tfx.argIf(outputPath, ["--output-path", outputPath]);
+            // Set vsix output path
+            const outputPath = tl.getInput("outputPath", false);
+            tfx.argIf(outputPath, ["--output-path", outputPath]);
 
-    // Before executing check update on tasks version
-    common.checkUpdateTasksVersion().then(() => {
-        const outputStream = new common.TfxJsonOutputStream(false);
+            // Before executing check update on tasks version
+            await common.checkUpdateTasksVersion();
+            const outputStream = new common.TfxJsonOutputStream(false);
 
-        tfx.exec(<any>{ outStream: outputStream, failOnStdErr: true }).then(code => {
-            const json = JSON.parse(outputStream.jsonString);
+            tfx.exec(<any>{ outStream: outputStream, failOnStdErr: true }).then(code => {
+                const json = JSON.parse(outputStream.jsonString);
 
-            if (outputVariable) {
-                tl.setVariable(outputVariable, json.path);
-            }
+                if (outputVariable) {
+                    tl.setVariable(outputVariable, json.path);
+                }
 
-            console.log(`Packaged extension: ${json.path}.`);
-            tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
-        }).fail(err => {
-            tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
-        }).finally(() => {
-            cleanupTfxArgs();
+                console.log(`Packaged extension: ${json.path}.`);
+                tl.setResult(tl.TaskResult.Succeeded, `tfx exited with return code: ${code}`);
+            }).fail(err => {
+                tl.setResult(tl.TaskResult.Failed, `tfx failed with error: ${err}`);
+            }).finally(() => {
+                cleanupTfxArgs();
+            });
         });
-    }).fail(err => {
+    }
+    catch (err) {
         tl.setResult(tl.TaskResult.Failed, `Error occurred while updating tasks version: ${err}`);
-    });
-});
+    }
+}
+
+run();
