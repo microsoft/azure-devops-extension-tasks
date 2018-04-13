4318cf2d7e102db604d7df0947d2286d8356c1b3
Jesse Houwing <jesse.houwing@gmail.com>
2018-04-13 09:48:24 +0200

    Features/server gate (#68)
    * Wait for server validation
    * ServerGate Expression syntax finally solved.
    * Support 'latest' to check last published version
    * Use servergate as template for agent based version
    * Updated icons
    * Leave empty to check the latest version
    * Marking IsValid Extension as preview
    * Support '' for latest in server gate

---
+++ BuildTasks/IsValidExtensionAgent/IsValidExtension.ts
@@ -0,0 +1,50 @@
+import * as tl from "vsts-task-lib/task";
+import * as common from "./common";
+import * as stream from "stream";
+import promiseRetry = require("promise-retry");
+
+// common.setProxy();
+
+function run() {
+    common.runTfx(tfx => {
+        tfx.arg(["version"]);
+        tfx.execSync();
+    });
+
+    common.runTfx(tfx => {
+        tfx.arg(["extension", "isvalid", "--json", "--no-color"]);
+
+        common.setTfxMarketplaceArguments(tfx);
+        common.validateAndSetTfxManifestArguments(tfx);
+
+        const options = {
+            retries: +tl.getInput("maxRetries", false) || 10,
+            factor: 1,
+            minTimeout: 1000 * (+tl.getInput("minTimeout", false) || 0),
+            maxTimeout: 1000 * 60 * (+tl.getInput("maxTimeout", false) || 15),
+            randomize: false
+        };
+
+        promiseRetry(options,
+            (retry, attempt) => {
+                tl.debug(`Attempt: ${attempt}`);
+                const result = tfx.execSync({ silent: false, failOnStdErr: false } as any);
+                const json = JSON.parse(result.stdout);
+                switch (json.status) {
+                case "pending":
+                    return retry(json.status);
+                case "success":
+                    return json.status;
+                default:
+                    throw json.status;
+                }
+        }).then(status => {
+            tl.setResult(tl.TaskResult.Succeeded, "Extension is valid.");
+            return status;
+        }).catch(err => {
+            tl.setResult(tl.TaskResult.Failed, `Extension validation failed: ${err}`);
+        });
+    });
+}
+
+run();
