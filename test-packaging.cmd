6c642ad237428914ccc6d44eac308a7b2b212ba8
Jesse Houwing <jesse.houwing@gmail.com>
2018-08-11 19:05:25 +0200

    rewrote task version and id generation

---
+++ test-packaging.cmd
@@ -0,0 +1,15 @@
+set INPUT_EXTENSIONID=ext
+set INPUT_EXTENSIONVERSION=8.9.10
+set INPUT_PUBLISHERID=me
+set INPUT_ROOTFOLDER=C:\Users\JesseHouwing\Source\Repos\vsts-ping-task-demo
+set INPUT_UPDATETASKSID=true
+set INPUT_UPDATETASKSVERSION=true
+set INPUT_UPDATETASKSVERSIONTYPE=minor
+
+set AGENT_WORKFOLDER=c:\temp
+
+npm run build:tasks
+
+pushd dist\BuildTasks\PackageExtension
+node packageextension.js
+popd
