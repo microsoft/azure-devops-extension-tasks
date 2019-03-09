c80c77aafacd96af344251145f1055cb139ce6bc
Jesse Houwing <jesse.houwing@gmail.com>
2019-03-09 18:09:48 +0100

    fix #118

--- BuildTasks/PublishVSExtension/PublishVSExtension.ts
+++ BuildTasks/PublishVSExtension/PublishVSExtension.ts
@@ -5,12 +5,11 @@ import * as util from "./Utils";
 let publisher = "";
 
 try {
-    let connectedService = tl.getInput("ConnectedServiceName", true);
+    let connectedService = tl.getInput("connectedServiceName", true);
     let token = tl.getEndpointAuthorizationParameter(connectedService, "password", true);
 
-    let vsixFile = tl.getPathInput("vsixFile", true, false);
-    let rootFolder = tl.getPathInput("rootFolder", false) || tl.getVariable("System.DefaultWorkingDirectory");
-    let manifestPattern = tl.getPathInput("manifestPattern", false, false) || "extension.manifest.json";
+    let vsixFile = tl.getPathInput("vsixFile", true, true);
+    let manifestFile = tl.getPathInput("manifestFile", true, true);
     let publisherId = tl.getInput("publisherId", true);
     publisher = publisherId;
     let ignoreWarnings = tl.getInput("ignoreWarnings", false);
@@ -18,17 +17,8 @@ try {
     console.info(`Logging in as '${publisherId}'`);
     util.login(publisherId, token);
 
-    console.info(`Searching for manifest file '${manifestPattern}' in '${rootFolder}'`);
-    let manifestPathList = tl.findMatch(rootFolder, manifestPattern);
-    if (manifestPathList && manifestPathList.length === 0) {
-        throw new Error("Could not find manifest file.");
-    }
-    let manifestPath = manifestPathList[0];
-
-    console.info(`Found manifest file: ${manifestPath} `)
-
     console.info(`Publishing '${vsixFile}' to Visual Studio marketplace`)
-    util.publish(vsixFile, manifestPath, ignoreWarnings);
+    util.publish(vsixFile, manifestFile, ignoreWarnings);
 } catch (error) {
     tl.error(error);
     tl.setResult(tl.TaskResult.Failed, error);
