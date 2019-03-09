fd0d84156a0086207f53afcfed50a353414277ba
Jesse Houwing <jesse.houwing@gmail.com>
2019-03-09 14:20:04 +0100

    Find vsixpublisher.exe usign vswhere
    Faster and more reliable.

--- BuildTasks/PublishVSExtension/Utils.ts
+++ BuildTasks/PublishVSExtension/Utils.ts
@@ -3,46 +3,28 @@ import tr = require("azure-pipelines-task-lib/toolrunner");
 import * as path from "path";
 
 let cacheVsixPublisherExe = "";
+let loggedIn = false;
 
 export function getVsixPublisherExe(): string {
-    if (cacheVsixPublisherExe == "") {
+    if (cacheVsixPublisherExe === "") {
         const vswhereTool = tl.tool(path.join(__dirname, "tools", "vswhere.exe"));
-        vswhereTool.line("-version [15.0,16.0) -latest -requires Microsoft.VisualStudio.Component.VSSDK -property installationPath");
-        let vsPath = vswhereTool.execSync({ silent: true } as tr.IExecSyncOptions).stdout;
-        vsPath = vsPath.trim();
-        tl.debug('Visual Studio 15.0 or higher installed path: ' + vsPath);
-        if (!isNullOrWhitespace(vsPath)) {
-            let vsixPublisherFolder = path.join(vsPath, 'VSSDK', 'VisualStudioIntegration', 'Tools', 'Bin');
-            if (!vsixPublisherFolder) {
-                let message = "Unable to find instance of Visual Studio with VSSDK installed...";
-                throw new Error(message);
-
-            }
-            tl.debug(`VSINSTALLDIR: '${vsixPublisherFolder}'`);
-            let vsixPublisherExe = path.join(vsixPublisherFolder, "VsixPublisher.exe");
-            let vsixPublisherExists = tl.which(vsixPublisherExe, true);
-            if (!vsixPublisherExists) {
-                let message = `Could not find VSIXPublisher.exe under '${vsixPublisherFolder}'`;
-                throw new Error(message);
-            }
-
-            tl.debug(`VSIXPublisher.exe path: '${vsixPublisherExe}'`);
-
+        vswhereTool.line("-version [15.0,17.0) -latest -requires Microsoft.VisualStudio.Component.VSSDK -find VSSDK\\VisualStudioIntegration\\Tools\\Bin\\VsixPublisher.exe");
+        const vswhereResult = vswhereTool.execSync({ silent: true } as tr.IExecSyncOptions);
+        const vsixPublisherExe = vswhereResult.stdout.trim();
+        if (vswhereResult.code === 0 && vsixPublisherExe && tl.exist(vsixPublisherExe))
+        {
+            tl.debug('VsixPublisher.exe installed path: ' + vsixPublisherExe);
             cacheVsixPublisherExe = vsixPublisherExe;
-            return vsixPublisherExe;
         }
+        else
+        {
+            throw new Error("Could not locate vsixpublisher.exe. Ensure the Visual Studio SDK is installed on the agent.");
+        }
+        return vsixPublisherExe;
     }
-    else {
+    else { 
         return cacheVsixPublisherExe;
     }
-    return null;
-}
-
-export function isNullOrWhitespace(input) {
-    if (typeof input === 'undefined' || input === null) {
-        return true;
-    }
-    return input.replace(/\s/g, '').length < 1;
 }
 
 export function login(publisher: string, token: string) {
@@ -58,21 +40,25 @@ export function login(publisher: string, token: string) {
         throw new Error("Login failed.");
     }
 
+    loggedIn = true;
     console.info(`Login successful.`)
 }
+
 export function logout(publisher: string) {
-    let vsixPublisherExe = getVsixPublisherExe();
-    let vsixPublisher = tl.tool(vsixPublisherExe);
-
-    vsixPublisher.arg("logout");
-    vsixPublisher.arg(["-publisherName", publisher]);
-    vsixPublisher.arg("-ignoreMissingPublisher");
-
-    if (vsixPublisher.execSync({ failOnStdErr: true } as tr.IExecOptions).code !== 0)
+    if (loggedIn)
     {
-        throw new Error("Logout failed.");
+        let vsixPublisherExe = getVsixPublisherExe();
+        let vsixPublisher = tl.tool(vsixPublisherExe);
+
+        vsixPublisher.arg("logout");
+        vsixPublisher.arg(["-publisherName", publisher]);
+        vsixPublisher.arg("-ignoreMissingPublisher");
+
+        if (vsixPublisher.execSync({ failOnStdErr: true } as tr.IExecOptions).code !== 0)
+        {
+            throw new Error("Logout failed.");
+        }
     }
-    
     console.info(`Logout successful.`)
 }
 
