ae0db34ccb4c727f0e10c5e5d328b3760a9c6e1a
Jesse Houwing <jesse.houwing@gmail.com>
2022-11-16 22:10:25 +0100

    Upgrading to Node 16
    # Conflicts:
    #	package-lock.json
    #	package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
         '@typescript-eslint/await-thenable': 'error', 
         '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
-        '@typescript-eslint/no-explicit-any': 'off'
-    }
+        '@typescript-eslint/no-explicit-any': 'off',
+        "@typescript-eslint/no-unused-vars": "off"
+    },
+    root: true
 };
