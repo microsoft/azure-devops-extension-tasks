e0fd393cd4532e76ae3cdfa26bee9c3a2195eaeb
Jesse Houwing <jesse.houwing@gmail.com>
2022-12-29 21:25:25 +0100

    Try faster compression when publishing from vsix by just extracting the manifest files

--- test-publishing.cmd
+++ test-publishing.cmd
@@ -1,32 +1,31 @@
-SET AGENT_WORKFOLDER=c:\temp
-SET SYSTEM_DEFAULTWORKINGDIRECTORY=C:\Users\jesse\source\repos\vsts-ping-task-demo
-set INPUT_CWD=C:\Users\jesse\source\repos\vsts-ping-task-demo
-set INPUT_ROOTFOLDER=C:\Users\jesse\source\repos\vsts-ping-task-demo
+SET AGENT_WORKFOLDER=%temp%
+SET SYSTEM_DEFAULTWORKINGDIRECTORY=C:\Users\JesseHouwing\source\azure-pipelines-variable-tasks\
+set INPUT_CWD=C:\Users\JesseHouwing\source\azure-pipelines-variable-tasks\
+set INPUT_ROOTFOLDER=C:\Users\JesseHouwing\source\azure-pipelines-variable-tasks\
+set INPUT_VSIXFILE=C:\Users\JesseHouwing\source\azure-pipelines-variable-tasks\jessehouwing.jessehouwing-vsts-variable-tasks-3.0.0.vsix
 SET INPUT_CONNECTTO=VsTeam
 SET INPUT_FILETYPE=vsix
 SET INPUT_EXTENSIONVISIBILITY=default
-SET INPUT_EXTENSIONVERSION=1.2.3
+SET INPUT_EXTENSIONVERSION=3.26.27
 SET INPUT_UPDATETASKSVERSION=true
-SET INPUT_UPDATETASKSVERSIONTYPE=major
+SET INPUT_UPDATETASKSVERSIONTYPE=minor
 SET INPUT_CONNECTEDSERVICENAME=A
 SET ENDPOINT_URL_A=https://marketplace.visualstudio.com
 SET ENDPOINT_AUTH_A={ "parameters": { "apitoken": "token", "username": "user", "password": "password" }, "Scheme": "basic" }
-SET INPUT_VSIXFILE=C:\Users\jesse\source\repos\vsts-ping-task-demo\jessehouwing.jessehouwing-vsts-ping-task-2.1.2.vsix
 
 set NODE_ENV=production
 set NO_UPDATE_NOTIFIER=true
 
+fnm use v16
+
 cmd /c "npm run build:tasks"
 
-pushd BuildTasks\TfxInstaller\TfxInstaller
-node TfxInstaller.js
-popd
+set __TFXPATH=C:\Users\JesseHouwing\AppData\Local\fnm_multishells\5548_1672346643184
 
-set __TFXPATH=c:\temp\agent\tools\tfx\0.7.11\x64
+pushd BuildTasks\PublishExtension\v4\PublishExtension\v4
+SET PREVIEW_FAST_UPDATE=false
+node PublishExtension.js
 
-
-pushd BuildTasks\PublishExtension\PublishExtension
-c:\TfsData\jessehouwing\externals.2.111.1\node\bin\node.exe PublishExtension.js
-c:\TfsData\jessehouwing\externals.2.136.1\node\bin\node.exe PublishExtension.js
+SET PREVIEW_FAST_UPDATE=true
 node PublishExtension.js
 popd
