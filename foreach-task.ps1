932e339cfe0c46c2631832aced195b89847b4bc3
Jesse Houwing <jesse.houwing@gmail.com>
2024-05-28 22:07:16 +0200

    Making v5 preview

--- foreach-task.ps1
+++ foreach-task.ps1
@@ -1,6 +1,8 @@
-(Get-ChildItem buildtasks\*\v*) | ForEach-Object{ 
+(Get-ChildItem buildtasks\*\v5) | ForEach-Object{ 
     write-host $_
     Push-Location $_
-    & npm audit fix
+    $task = gc -raw task.json | convertfrom-json -AsHashtable
+    $task.preview = $true
+    $task | convertto-json -depth 100 | set-content task.json
     Pop-Location
 }
