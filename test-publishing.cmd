93d460eb98a21deb302895af2bb9e114ec78473c
Jesse Houwing <jesse.houwing@gmail.com>
2025-02-28 13:19:42 +0100

    more potential async fixes

--- test-publishing.cmd
+++ test-publishing.cmd
@@ -5,7 +5,7 @@ set INPUT_VSIXFILE=E:\azure-devops-extension-tasks\ms-devlabs.vsts-developer-too
 SET INPUT_CONNECTTO=VsTeam
 SET INPUT_FILETYPE=vsix
 SET INPUT_EXTENSIONVISIBILITY=default
-SET INPUT_EXTENSIONVERSION=3.26.27
+SET INPUT_EXTENSIONVERSION=3.26.23
 SET INPUT_UPDATETASKSVERSION=true
 SET INPUT_UPDATETASKSVERSIONTYPE=minor
 SET INPUT_CONNECTEDSERVICENAME=A
@@ -24,16 +24,16 @@ md %AGENT_WORKFOLDER%
 md %AGENT_TOOLSDIRECTORY%
 md %AGENT_TEMPDIRECTORY%
 
-fnm use v16
+fnm use v20
 
 cmd /c "npm run build:tasks"
 
 set __TFXPATH=C:\Users\JESSEH~1\AppData\Local\Temp\agent\tools\tfx\0.18.0\x64\
 
-pushd BuildTasks\PublishExtension\v4\PublishExtension\v4
-SET PREVIEW_FAST_UPDATE=false
+pushd BuildTasks\PublishExtension\v4\PublishExtension\v5
 node PublishExtension.js
+popd
 
-SET PREVIEW_FAST_UPDATE=true
+pushd BuildTasks\PublishExtension\v4\PublishExtension\v4
 node PublishExtension.js
 popd
