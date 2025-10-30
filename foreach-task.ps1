b368250b9b2407e39c618f2caaa359edf6c59a9e
Jesse Houwing <jesse.houwing@gmail.com>
2025-10-30 14:13:43 -0700

    Updating to latest node 20

--- foreach-task.ps1
+++ foreach-task.ps1
@@ -1,8 +1,9 @@
 (Get-ChildItem buildtasks\*\v*) | ForEach-Object { 
     write-host $_
     Push-Location $_
-    if ((gc -raw .\package.json -ErrorAction Ignore) -like "*@types/node*") { 
-        npm install @types/node@^20 --save-dev
-    }
+    #if ((gc -raw .\package.json -ErrorAction Ignore) -like "*@types/node*") { 
+    #    npm install @types/node@^20.19.23 --save-dev
+    #}
+    npm audit
     Pop-Location
 }
