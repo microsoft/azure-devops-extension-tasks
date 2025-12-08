92989aa74e2fccca81c387b92ab8b6b4804d1f5f
Jesse Houwing <jesse.houwing@gmail.com>
2025-12-08 12:08:28 +0100

    Upgrade embedded tfx-cli

--- Scripts/foreach-task.ps1
+++ Scripts/foreach-task.ps1
@@ -1,9 +1,10 @@
-(Get-ChildItem buildtasks\*\v*) | ForEach-Object { 
+
+(Get-ChildItem buildtasks\* -Directory) | ForEach-Object { 
     write-host $_
     Push-Location $_
     #if ((gc -raw .\package.json -ErrorAction Ignore) -like "*@types/node*") { 
     #    npm install @types/node@^20.19.23 --save-dev
     #}
-    npm audit
+    npm audit fix
     Pop-Location
 }
